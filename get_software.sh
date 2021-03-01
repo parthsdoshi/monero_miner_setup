e5b3b10497cb05dd18ac326ea1bded14b98da5a2
Parth Doshi <doshi.parth9@gmail.com>
2021-02-28 20:07:53 -0800

    fix untarring

--- get_software.sh
+++ get_software.sh
@@ -1,5 +1,5 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-bionic-x64.tar.gz ./soft.tar.gz
-tar -xvf soft.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-bionic-x64.tar.gz
+tar -xvf xmrig-6.9.0-bionic-x64.tar.gz -C soft/
 cp ./config.json soft/config.json
 cd soft
 ./xmrig
---
+++ startup_script.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/parthsdoshi/monero_miner_setup.git
+cd monero_miner_setup
+bash get_software.sh
