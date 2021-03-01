c8912f48197bf9429dd266d8821911cbdbbfa4c4
Parth Doshi <doshi.parth9@gmail.com>
2021-03-01 00:57:44 -0800

    update script

--- get_software.sh
+++ get_software.sh
@@ -1,5 +1,10 @@
+echo "Grabbing XMRig"
 wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-bionic-x64.tar.gz
+echo "Unpacking XMRig"
 tar -xvf xmrig-6.9.0-bionic-x64.tar.gz
+echo "Copying config"
 cp ./config.json xmrig-6.9.0/config.json
 cd xmrig-6.9.0
+echo "Starting XMRig with config:"
+cat config.json
 ./xmrig
