fc84a23d365f1584ea0852ebf59bf29ff8e0ab12
Parth Doshi <doshi.parth9@gmail.com>
2021-02-28 20:10:37 -0800

    stop unpacking to dir

--- get_software.sh
+++ get_software.sh
@@ -1,5 +1,6 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-bionic-x64.tar.gz
-tar -xvf xmrig-6.9.0-bionic-x64.tar.gz -C soft/
-cp ./config.json soft/config.json
+mkdir soft/
+tar -xvf xmrig-6.9.0-bionic-x64.tar.gz
+cp ./config.json xmrig-6.9.0/config.json
 cd soft
 ./xmrig
