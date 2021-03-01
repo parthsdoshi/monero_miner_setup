ebd2296c2d6b6c34108be3302357b0ff28569979
Parth Doshi <doshi.parth9@gmail.com>
2021-02-28 20:46:59 -0800

    add sudo

--- get_software.sh
+++ get_software.sh
@@ -1,6 +1,5 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-bionic-x64.tar.gz
-mkdir soft/
 tar -xvf xmrig-6.9.0-bionic-x64.tar.gz
 cp ./config.json xmrig-6.9.0/config.json
 cd xmrig-6.9.0
-./xmrig
+sudo ./xmrig
