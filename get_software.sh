d65f60c0e5cb6677556f8af9f1d04a95cf3810d0
Parth Doshi <doshi.parth9@gmail.com>
2021-02-28 20:11:36 -0800

    fix cd

--- get_software.sh
+++ get_software.sh
@@ -2,5 +2,5 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-bionic-
 mkdir soft/
 tar -xvf xmrig-6.9.0-bionic-x64.tar.gz
 cp ./config.json xmrig-6.9.0/config.json
-cd soft
+cd xmrig-6.9.0
 ./xmrig
