cff0c813e15ae2dfdd195b47bfc9423893b3b653
Parth Doshi <doshi.parth9@gmail.com>
2021-03-01 01:34:58 -0800

    use monero ocean instead

--- get_software.sh
+++ get_software.sh
@@ -1,7 +1,8 @@
 echo "Grabbing XMRig"
-wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-bionic-x64.tar.gz
+# wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-bionic-x64.tar.gz
+wget https://github.com/MoneroOcean/xmrig/releases/download/v6.9.0-mo1/xmrig-v6.9.0-mo1-lin64.tar.gz
 echo "Unpacking XMRig"
-tar -xvf xmrig-6.9.0-bionic-x64.tar.gz
+tar -xvf xmrig-6.9.0-bionic-x64.tar.gz -C xmrig-6.9.0/
 echo "Copying config"
 cp ./config.json xmrig-6.9.0/config.json
 cd xmrig-6.9.0
