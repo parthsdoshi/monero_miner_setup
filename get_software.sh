2ca03a6d7b7e752bdef37242c1f7f5fa8e395be5
Parth Doshi <doshi.parth9@gmail.com>
2021-03-02 01:27:37 -0800

    try mining switcher

--- get_software.sh
+++ get_software.sh
@@ -1,13 +1,24 @@
+echo "Updating system resource pointers"
+sudo apt update
+echo "Installing node"
+sudo apt install -y nodejs
+
+echo "Grabbing multi-miner"
+wget https://raw.githubusercontent.com/MoneroOcean/meta-miner/master/mm.js
+chmod +x mm.js
+
 echo "Grabbing XMRig"
 wget https://github.com/xmrig/xmrig/releases/download/v6.9.0/xmrig-6.9.0-bionic-x64.tar.gz
-# wget https://github.com/MoneroOcean/xmrig/releases/download/v6.9.0-mo1/xmrig-v6.9.0-mo1-lin64.tar.gz
 echo "Unpacking XMRig"
 tar -xvf xmrig-6.9.0-bionic-x64.tar.gz
-# mkdir xmrig-6.9.0/
-# tar -xvf xmrig-v6.9.0-mo1-lin64.tar.gz -C xmrig-6.9.0/
-echo "Copying config"
-cp ./config.json xmrig-6.9.0/config.json
+
+echo "Copying files"
+cp ./config.json xmrig-6.9.0/
+cp ./mm.js xmrig-6.9.0/
 cd xmrig-6.9.0
-echo "Starting XMRig with config:"
+
+echo "Starting with config:"
 cat config.json
-sudo --preserve-env=AZ_BATCH_NODE_ID ./xmrig
+
+sudo node mm.js -p=gulf.moneroocean.stream:10128 -m "sudo --preserve-env=AZ_BATCH_NODE_ID ./xmrig --config=config.json"
+# sudo --preserve-env=AZ_BATCH_NODE_ID ./xmrig
